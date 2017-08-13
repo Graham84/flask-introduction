82286f296adb5507e2284060df209c621e454764
Graham84 <gosiochain@rocketmail.com>
2017-08-13 18:44:09 +0100

    13-08-17
    13-08-17

--- flask_introduction/library/templates/_00_setup virtual env.py
+++ flask_introduction/library/templates/_00_setup virtual env.py
@@ -2,9 +2,13 @@
 cd to correct file
 python -m virtualenv flask_introduction
 
-cd to flask_introduction
+activate app
 .\scripts\activate
 
 install requirements
+pip install -r requirements.txt
+
+run app
+python flask_introduction/run_app.py
 
 '''
